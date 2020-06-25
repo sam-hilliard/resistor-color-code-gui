a9bec354169b30e91b1a13960711d8932276fd4b
Sam Hilliard <samhilliard51@gmail.com>
2020-06-24 21:14:17 -0700

    Fixed major bugs
    Now calculates values with complete success. Just need to format output and make more asthetically appealing.

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ class App:
     def __init__(self):
         self.window = tk.Tk()
         self.fr_content = tk.Frame(self.window)
+        self.menus = []
         self.menu_vars = []
+        self.lbl_result = ttk.Label(self.fr_content)
+        self.option = tk.IntVar()
 
     # displays the gui of the application
     def run(self):
@@ -23,23 +26,51 @@ class App:
         self.fr_content.pack(fill='both', expand=True)
         self.configMenus()
 
+        # radio button to select general purpose resistor calculation
+        rb_general = ttk.Radiobutton(
+            self.fr_content, text='General Purpose', variable=self.option, value=0, command=self.configMenus)
+        rb_general.grid(row=1, column=0)
+
+        # radio button to select precision resistor calculation
+        rb_precision = ttk.Radiobutton(
+            self.fr_content, text='Precision', variable=self.option, value=1, command=self.configMenus)
+        rb_precision.grid(row=1, column=1)
+
+        # displays the numerical value of the resistor based on user input
+        self.lbl_result.configure(text='Resistor value: None')
+        self.lbl_result.grid(row=2, column=0)
+
         tk.mainloop()
 
     # adds the drop down menus to the window
     def configMenus(self):
+        # make sure no extra menus are left on the screen when switching between modes
+        if len(self.menu_vars) > 0:
+            for menu in self.menus:
+                menu.destroy()
+            self.menu_vars.clear()
+            self.menus.clear()
+
+        # makes sure label doesn't display previous value when switching options
+        self.lbl_result.configure(text='Resistor value: None')
+
+        # all possible colors to select from
         default_options = ['black', 'brown', 'red', 'orange', 'yellow',
                            'green', 'blue', 'violet', 'grey', 'white', 'gold',
-                           'silver', 'none']
+                           'silver']
 
+        # creates up to six drop downs where user will select band color from
         for i in range(6):
+            # set so that the 'none' is both selected and a list item
             options = ['none', 'none']
             for j in range(len(default_options)):
                 add_option = False
+                # color code chart does not make use of every default color option for each section (tolerance, multiplier, etc...)
                 if i == 3:
                     if j <= 6 or (j >= 10 and j <= 11):
                         add_option = True
                 elif i == 4:
-                    if (j > 0 and j < 3) or (j > 4 and j < 8) or (j > 9 and j < 12):
+                    if (j > 0 and j < 3) or (j > 4 and j < 9) or (j > 9 and j < 12):
                         add_option = True
                 elif i == 5:
                     if j < 9:
@@ -48,20 +79,47 @@ class App:
                     if j < 10:
                         add_option = True
 
+                # only colors that correspond to the resistor code chart are adde
                 if add_option:
                     options.append(default_options[j])
+
+            if self.option.get() == 0 and (i == 2 or i == 5):
+                continue
+
             menu_var = tk.StringVar(self.fr_content)
             menu = ttk.OptionMenu(self.fr_content, menu_var, *options)
-            menu.grid(row=1, column=i, padx=5)
+            menu.grid(row=0, column=i, padx=5)
+            self.menus.append(menu)
             self.menu_vars.append(menu_var)
             menu_var.trace_add('write', self.calculate)
 
     # interprets the number values of the drop downs to a numerical value
     def calculate(self, var, indx, mode):
         bands = []
+        isValid = False
 
-        for var in self.menu_vars:
-            bands.append(var.get())
+        # adds usable input from each drop down menu
+        for menu_var in self.menu_vars:
+            if menu_var.get() != 'none':
+                bands.append(menu_var.get())
+            else:
+                break
+        
+        # determines if input given follows resistor color code
+        if len(bands) == 1:
+            if bands[0] == 'black':
+                isValid = True
+        else:
+            if len(bands) > 2 and len(self.menus) - len(bands) <= 1:
+                isValid = True
 
-        resistor = Resistor(bands)
-        numBands = resistor.getNumBands()
+        if isValid:
+            resistor =  Resistor(bands, True) if len(self.menus) > 4 else Resistor(bands, False)
+
+            value = resistor.getDigits() * (10 ** resistor.getMultiplier())
+            result = str(value) + '\u03A9 ' + resistor.getTolerance() + resistor.getTempCo()
+
+            self.lbl_result.configure(text=f'Resistor value: {result}')
+
+        else:
+            self.lbl_result.configure(text='Resitor value: Invalid')
--- resistor.py
+++ resistor.py
@@ -1,43 +1,58 @@
 class Resistor:
 
-    def __init__(self, bands):
+    def __init__(self, bands, isPrecision):
         self.bands = bands
+        self.isPrecision = isPrecision
 
     # determines the digits based on bands 1-3
     def getDigits(self):
-        print('getting digits')
+        colorCode = {'black': '0', 'brown': '1', 'red': '2', 'orange': '3', 'yellow': '4',
+                     'green': '5', 'blue': '6', 'violet': '7', 'grey': '8', 'white': '9'}
+        digits = 0
+        numDigits = 3 if self.isPrecision else 2
+        strDigits = ''
+
+        if len(self.bands) > 1:
+            for i in range(numDigits):
+                strDigits += colorCode[self.bands[i]]
+            digits = int(strDigits)
+        
+
+        return digits
 
     # determines the mulitplier based on bands 3-4
     def getMultiplier(self):
-        print('getting the multiplier')
+        colorCode = {'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4,
+                     'green': 5, 'blue': 6, 'violet': 7, 'grey': 8, 'white': 9, 'gold': -1, 'silver': -2}
+        multNum = 3 if self.isPrecision else 2
+
+        return colorCode[self.bands[multNum]]
 
     # determines the tolerance based 4-5
     def getTolerance(self):
-        print('gettting tolerance')
+        colorCode = {'brown': '1%', 'red': '2%', 'green': '0.50%', 'blue': '0.25%',
+                     'violet': '0.10%', 'grey': '0.05%', 'gold': '5%', 'silver': '10%'}
+        tolerance = ''
+
+        if self.isPrecision:
+            tolerance = colorCode[self.bands[4]]
+        else:
+            if len(self.bands) > 3:
+                tolerance = colorCode[self.bands[3]]
+            else:
+                tolerance = '20%'
+
+        return '\u00B1' + tolerance
+
 
     # determines the temperature coefficient based on band 6
     def getTempCo(self):
-        print('getting the temp coefficient')
+        colorCode = {'black': '250', 'brown': '100', 'red': '50', 'orange': '15', 'yellow': '25',
+                     'green': '20', 'blue': '10', 'violet': '5', 'grey': '1'}
 
-    def getNumBands(self):
-        bandNum = 0
-        isNone = False
+        if self.isPrecision and len(self.bands) > 5:
+            return colorCode[self.bands[5]] + 'ppm/K'
         
+        return ''
 
-        for band in self.bands:
-            if band == 'none':
-                isNone = True
-            else:
-                if not isNone:
-                    bandNum += 1
-                else:
-                    bandNum = -1
-
-        if bandNum == 1 and self.bands[0] != 'black':
-            return -1
-
-
-        return bandNum
-
-    def toString(self):
-        return 'hi'
+        
